a689feff51c66ea0a43200bc14c60089c5a79741
Jagdeep Bhatia <jagdeepsb150@gmail.com>
2022-06-09 12:54:35 -0700

    Autoreload test

--- src/thing.py
+++ src/thing.py
@@ -1,7 +1,8 @@
 class Thing():
-    def __init__(self, a, b, c=5) -> None:
+    def __init__(self, a, b, c=5, d=5) -> None:
         self.a = a
         self.b = b
         self.c = c
+        self.d = d
     
     
