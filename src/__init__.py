fdee5a4ddb144bf526b9b1b74e4a23760674f439
Jagdeep Bhatia <jagdeepsb150@gmail.com>
2022-06-10 23:26:53 -0700

    Add constants file for testing

--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-# from project.src.thing import Thing
--- src/thing.py
+++ src/thing.py
@@ -1,8 +1,18 @@
+from utils.constants import THIS_IS_A_CONSTANT
 class Thing():
     def __init__(self, a, b, c=5, d=5) -> None:
         self.a = a
         self.b = b
         self.c = c
         self.d = d
+        self.e = THIS_IS_A_CONSTANT
     
-    
+if __name__ == "__main__":
+    x = Thing(1, 2)
+    print(
+        x.a,
+        x.b,
+        x.c,
+        x.d,
+        x.d
+    )
