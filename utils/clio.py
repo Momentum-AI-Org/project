69be2f04446450fdac26bd90579b26b6261eae25
Jagdeep Bhatia <jagdeepsb150@gmail.com>
2022-06-20 17:35:34 -0700

    Debug

--- utils/clio.py
+++ utils/clio.py
@@ -4,4 +4,4 @@ from subprocess import PIPE, run
 def run_command(command: str) -> str:
     """Run command on google colab machine and get output"""
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    return result.stdout
+    return result.stdout, result.stderr
